f4ccef1a39eed8ba9bc21cd8c921d88300ece15b
Aishwarya6070 <90592693+Aishwarya6070@users.noreply.github.com>
2021-11-24 16:25:22 +0530

    Update Maps.java

--- Day2/src/day2/Maps.java
+++ Day2/src/day2/Maps.java
@@ -7,9 +7,9 @@ public class Maps {
     public static void main(String[] args) {
         Map<String, String> map = new HashMap<String, String>();
 
-        map.put("Aaryan", "123");
-        map.put("Abhay", "12345679");
-        map.put("Akash", "12345679");
+        map.put("Abhi", "123");
+        map.put("Arun", "1234579");
+        map.put("Aishu", "12345679");
 
         map.forEach((k, v) -> System.out.println(k + " : " + v));
     }
